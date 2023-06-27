63e3a858c4f3d7656f27408a5bb8675034edf377
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-06-27 16:05:46 +0300

    update index.js - basename

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          {/* <BrowserRouter basename="/goit-react-hw-08-movies"> */}
+        <BrowserRouter basename="/goit-react-hw-08-movies">
           <App />
         </BrowserRouter>
       </PersistGate>
